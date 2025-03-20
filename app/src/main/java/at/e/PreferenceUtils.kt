3890b6dd619549120bdf06d71113c2eb742eb749
PastaCodes <marco.j.buda@gmail.com>
2025-03-20 19:14:19 +0100

    Fully async preferences

--- app/src/main/java/at/e/PreferenceUtils.kt
+++ app/src/main/java/at/e/PreferenceUtils.kt
@@ -5,7 +5,10 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
 object PreferenceKeys {
     val FindTablePreferredMethod = intPreferencesKey("find_table_preferred_method")
@@ -14,5 +17,8 @@ object PreferenceKeys {
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 suspend fun <T> Context.loadPreference(key: Preferences.Key<T>, defaultValue: T? = null, consumer: (T?) -> Unit) {
-    dataStore.data.map { it[key] ?: defaultValue }.collect(consumer)
+    withContext(Dispatchers.IO) {
+        val value = dataStore.data.map { it[key] ?: defaultValue }.first()
+        consumer(value)
+    }
 }
